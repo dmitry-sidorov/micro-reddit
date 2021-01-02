9217f3e8ec71cb521b2881e45c3d28f180c76f4f
Dmitriy Sidorov <d_sidorov@wargaming.net>
2021-01-03 00:50:49 +0300

    Add user_id to posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :username, presence: true, length: { minimum: 6, maximum: 50 }
+
   has_many :posts
   has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_02_212359) do
+ActiveRecord::Schema.define(version: 2021_01_02_214007) do
 
   create_table "comments", force: :cascade do |t|
     t.string "text"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_01_02_212359) do
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_id"
   end
 
   create_table "users", force: :cascade do |t|
