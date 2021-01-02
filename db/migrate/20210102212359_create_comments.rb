b283b7a1b2a245a4714ade369373cc0fb73b55ed
Dmitriy Sidorov <d_sidorov@wargaming.net>
2021-01-03 01:20:29 +0300

    Add comment validations

--- db/migrate/20210102212359_create_comments.rb
+++ db/migrate/20210102212359_create_comments.rb
@@ -2,6 +2,8 @@ class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
       t.string :text
+      t.belongs_to :user
+      t.belongs_to :post
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,12 @@ ActiveRecord::Schema.define(version: 2021_01_02_212359) do
 
   create_table "comments", force: :cascade do |t|
     t.string "text"
+    t.integer "user_id"
+    t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
